d37231832c107e560aaba2da26f9791bd8d5ca6a
Zexi Han <zexihan@me.com>
2019-02-24 18:33:07 -0500

    styled the overlay panel

--- src/components/MapOverlay.js
+++ src/components/MapOverlay.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as d3 from 'd3';
 
-import $ from 'jquery';
-
-import * as d3 from "d3";
+import '../css/MapOverlay.css';
 
 const styleMapOverlayContainer = {
   position: "absolute",
@@ -105,12 +104,12 @@ class MapOverlay extends Component {
           <div className='map-overlay-container' style={styleMapOverlayContainer}>
             <div className='map-overlay' style={styleMapOverlay}>
               <h2 className="text-center my-2">New York City</h2>
-              <p className="text-center">Such a beautiful disease</p>
+              <p className="text-center" style={{ "fontStyle": "italic" }}>Such a beautiful disease</p>
               <Link to={`/`}>
                 <button type="button" className="btn btn-dark btn-block mb-2">Home</button>
               </Link>
               <div className="input-group my-2">
-                <input type="text" id="search" className="form-control" placeholder="Search..." onChange={this.searchInputChanged} />
+                <input type="text" id="search" className="form-control" placeholder="Search neighborhood..." onChange={this.searchInputChanged} />
                 <div className="input-group-append">
                   <button className="btn btn-outline-secondary" type="button" onClick={this.search}><i className="fas fa-search"></i></button>
                 </div>
@@ -144,36 +143,40 @@ class MapOverlay extends Component {
               </div>
               <hr />
               <div>
-                <h4>Neighborhood</h4>
+                <h4>Neighborhood view</h4>
                 <h4 id='location-title'>{this.props.clickedNbh}</h4>
                 <p id='location-description'>{this.props.clickedNbhGrp}</p>
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                   <li className="nav-item">
-                    <a className="nav-link active" onClick={this.selectTab} id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                    <a className="nav-link active" onClick={this.selectTab} id="overview-tab" data-toggle="tab" href="#overview-panel" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Entertainment</a>
+                    <a className="nav-link" onClick={this.selectTab} id="entertainment-tab" data-toggle="tab" href="#entertainment-panel" role="tab" aria-controls="entertainment" aria-selected="false">Entertainment</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Expense</a>
+                    <a className="nav-link" onClick={this.selectTab} id="expense-tab" data-toggle="tab" href="#expense-panel" role="tab" aria-controls="expense" aria-selected="false">Expense</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Host</a>
+                    <a className="nav-link" onClick={this.selectTab} id="host-tab" data-toggle="tab" href="#host-panel" role="tab" aria-controls="host" aria-selected="false">Host</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Noise</a>
+                    <a className="nav-link" onClick={this.selectTab} id="noise-tab" data-toggle="tab" href="#noise-panel" role="tab" aria-controls="noise" aria-selected="false">Noise</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Safety</a>
+                    <a className="nav-link" onClick={this.selectTab} id="safety-tab" data-toggle="tab" href="#safety-panel" role="tab" aria-controls="safety" aria-selected="false">Safety</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" onClick={this.selectTab} id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Transit</a>
+                    <a className="nav-link" onClick={this.selectTab} id="transit-tab" data-toggle="tab" href="#transit-panel" role="tab" aria-controls="transit" aria-selected="false">Transit</a>
                   </li>
                 </ul>
                 <div className="tab-content" id="myTabContent">
-                  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-                  <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-                  <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                  <div className="tab-pane fade show active" id="home-panel" role="tabpanel" aria-labelledby="home-tab">Neighborhood overview.</div>
+                  <div className="tab-pane fade" id="entertainment-panel" role="tabpanel" aria-labelledby="entertainment-tab">Entertainment details.</div>
+                  <div className="tab-pane fade" id="expense-panel" role="tabpanel" aria-labelledby="expense-tab">Expense details.</div>
+                  <div className="tab-pane fade" id="host-panel" role="tabpanel" aria-labelledby="host-tab">Host details.</div>
+                  <div className="tab-pane fade" id="noise-panel" role="tabpanel" aria-labelledby="noise-tab">Noise details.</div>
+                  <div className="tab-pane fade" id="safety-panel" role="tabpanel" aria-labelledby="safety-tab">Safety details.</div>
+                  <div className="tab-pane fade" id="transit-panel" role="tabpanel" aria-labelledby="transit-tab">Transit details.</div>
                 </div>
               </div>
             </div>
